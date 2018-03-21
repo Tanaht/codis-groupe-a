55e7b53769d9c3a58abcd73d304ece58873a9d6b
Kévin Le Bon <kevin.le_bon@yahoo.fr>
2018-03-21 15:52:34 +0100

    Write a RabbitMQ configuration class

---
+++ server/src/main/java/fr/istic/sit/codisgroupea/rabbitmq/RabbitConfiguration.java
@@ -0,0 +1,56 @@
+package fr.istic.sit.codisgroupea.rabbitmq;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for RabbitMQ client.
+ */
+@Configuration
+public class RabbitConfiguration {
+    private static String SERVER_HOSTNAME = "lapommevolante.istic.univ-rennes1.fr";
+    private static int SERVER_RABBITMQ_PORT = 8081;
+    private static String SERVER_RABBITMQ_USER = "admin";
+
+    /* Very temporary */
+    private static String SERVER_RABBITMQ_PSSWD = "admin";
+
+    /**
+     * Create a new connection factory.
+     *
+     * @return the newly created connection factory.
+     */
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory cf = new CachingConnectionFactory(SERVER_HOSTNAME, SERVER_RABBITMQ_PORT);
+        cf.setUsername(SERVER_RABBITMQ_USER);
+        cf.setPassword(SERVER_RABBITMQ_PSSWD);
+
+        return cf;
+    }
+
+    /**
+     * Create a new AMQP admin.
+     *
+     * @return the newly created AMQP admin.
+     */
+    @Bean
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
+
+    /**
+     * Create a new rabbit template.
+     *
+     * @return the newly created rabbit tamplate.
+     */
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        return new RabbitTemplate(connectionFactory());
+    }
+}
