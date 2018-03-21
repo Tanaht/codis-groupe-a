a978e13d638ad5d3ffb8034167d5b95cf0fe6f7a
Unknown <beaulielloic@gmail.com>
2018-03-21 09:42:24 +0100

    init project

---
+++ server/src/main/java/fr/istic/sit/codisgroupea/CodisGroupeAApplication.java
@@ -0,0 +1,12 @@
+package fr.istic.sit.codisgroupea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodisGroupeAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodisGroupeAApplication.class, args);
+	}
+}
