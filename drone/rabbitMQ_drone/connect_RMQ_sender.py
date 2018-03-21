b1c53603201c88a1d7a17e19b2ae4c3972d71dd7
Abdel <ghani.merzouk@gmail.com>
2018-03-21 23:40:08 +0100

    commentaires explication connexion rabbitMQ

--- drone/rabbitMQ_drone/connect_RMQ_sender.py
+++ drone/rabbitMQ_drone/connect_RMQ_sender.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
+# coding=utf-8
 import pika
 
-# Set the connection parameters to connect to lapommevolante.istic.univ-rennes1.fr on port 5672
-# on the / virtual host using the username "admin" and password "admin"
+# Etablir une connexion avec le serveur RabbitMQ :
+# host  "lapommevolante.istic.univ-rennes1.fr" and  port 5672
+# username "admin" and password "admin"
 credentials = pika.PlainCredentials('admin', 'admin')
 parameters = pika.ConnectionParameters('lapommevolante.istic.univ-rennes1.fr',
                                        8081,
                                        '/',
                                        credentials)
-                                       
+
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 
-
+# Creons une queue "hello" à laquelle le message sera livré:
 channel.queue_declare(queue='hello')
 
+# Dans RabbitMQ, un message ne peut jamais être envoyé directement à la queue, il doit toujours passer par "exchange"
+# Specifier le nom de la queue de destination dans "exchange" dans le parametre "routing_key"
 channel.basic_publish(exchange='',
                       routing_key='hello',
                       body='Hello World!')
 print(" [x] Sent 'Hello World!'")
+
+# Message envoye et connexion fermee
 connection.close()
+
