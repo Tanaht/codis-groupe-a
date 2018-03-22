3c5d6d2289d72ec6abd334222e70def0dde0e404
Kévin Le Bon <kevin.le_bon@yahoo.fr>
2018-03-22 16:36:12 +0100

    [US-1][#21] Add Javadoc for User and Role entities

--- server/src/main/java/fr/istic/sit/codisgroupea/model/entity/User.java
+++ server/src/main/java/fr/istic/sit/codisgroupea/model/entity/User.java
@@ -3,48 +3,88 @@ package fr.istic.sit.codisgroupea.model.entity;
 import javax.persistence.*;
 import java.util.List;
 
+/**
+ * The type User.
+ */
 @Entity
 public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String username;
-
     private String password;
 
     @OneToMany
     private List<Role> roles;
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Gets username.
+     *
+     * @return the username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets username.
+     *
+     * @param username the username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Gets password.
+     *
+     * @return the password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets password.
+     *
+     * @param password the password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Gets roles.
+     *
+     * @return the roles
+     */
     public List<Role> getRoles() {
         return roles;
     }
 
+    /**
+     * Sets roles.
+     *
+     * @param roles the roles
+     */
     public void setRoles(List<Role> roles) {
         this.roles = roles;
     }
