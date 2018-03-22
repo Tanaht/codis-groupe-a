c775edecf3aac4561feed7291587422acb7736e6
Loïc <beaulielloic@gmail.com>
2018-03-22 10:26:38 +0100

    Removing node_modules from git history

--- server/src/main/java/fr/istic/sit/codisgroupea/rabbitmq/RabbitmqConsumer.java
+++ server/src/main/java/fr/istic/sit/codisgroupea/rabbitmq/RabbitmqConsumer.java
@@ -1,6 +1,5 @@
 package fr.istic.sit.codisgroupea.rabbitmq;
 
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -8,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class RabbitmqConsumer {
 
     @RabbitListener(queues = {RabbitmqConstante.queueTestFanout})
-    public void receiveMessageFromFanout1(String message) {
-        System.out.println(message);
+    public void receiveMessageFromFanout1(byte[] message) {
+        System.out.println(new String(message));
     }
 }
