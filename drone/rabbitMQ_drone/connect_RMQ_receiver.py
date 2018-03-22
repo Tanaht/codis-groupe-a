62e912ba078365e4dc11b239bde00144906f3c8a
Abdel <ghani.merzouk@gmail.com>
2018-03-22 08:47:37 +0100

    Ajout commentaires du receiver drone/rabbitMQ

--- drone/rabbitMQ_drone/connect_RMQ_receiver.py
+++ drone/rabbitMQ_drone/connect_RMQ_receiver.py
@@ -17,13 +17,15 @@ channel = connection.channel()
 # Creons une queue "hello" à laquelle le message sera livré:
 channel.queue_declare(queue='hello')
 
+# Une fonction "callback" souscrit a une "queue", lors d'une reception de message elle est appelée
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
-
+# Indiquer à RabbitMQ que cette fonction "callback" doit recevoir des messages de la "queue" hello
 channel.basic_consume(callback,
                     queue='hello',
                     no_ack=True)
 
+# Une boucle sans fin qui attend des données et exécute des rappels si nécessaire.
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
 
