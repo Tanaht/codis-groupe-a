319251f97a04ebf4977622b6672023143be30dd5
Jordan Maheu <jmaheu1995@gmail.com>
2018-03-23 10:18:20 +0100

    [US-1][#18] Add three socket controller

---
+++ server/src/main/java/fr/istic/sit/codisgroupea/controller/DroneSocketController.java
@@ -0,0 +1,12 @@
+package fr.istic.sit.codisgroupea.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Controller for drone routes
+ * drone/...
+ * interventions/drone/...
+ */
+@Controller
+public class DroneSocketController {
+}
