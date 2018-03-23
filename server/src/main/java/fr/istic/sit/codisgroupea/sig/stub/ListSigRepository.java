b7a10f2b8f5df1fae3081dd74435cef0a01746e8
Kévin Le Bon <kevin.le_bon@yahoo.fr>
2018-03-23 15:15:17 +0100

    [US-3][#31] Begin SIG population

--- server/src/main/java/fr/istic/sit/codisgroupea/sig/stub/ListSigRepository.java
+++ server/src/main/java/fr/istic/sit/codisgroupea/sig/stub/ListSigRepository.java
@@ -1,17 +1,38 @@
 package fr.istic.sit.codisgroupea.sig.stub;
 
-import fr.istic.sit.codisgroupea.model.entity.Position;
-import fr.istic.sit.codisgroupea.model.entity.SymbolSitac;
+import fr.istic.sit.codisgroupea.model.entity.*;
 import fr.istic.sit.codisgroupea.sig.SigRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * List SIG repository.
+ */
 public class ListSigRepository implements SigRepository<SymbolSitac> {
     private List<SymbolSitac> symbols;
 
+    /**
+     * Instantiates a new List SIG repository.
+     */
     public ListSigRepository() {
         symbols = new ArrayList<>();
+
+        addToSig(Color.BLUE, Shape.CIRCLE, null, new Position(48.11540, -1.63813));
+        addToSig(Color.ORANGE, Shape.TRIANGLEDOWN, null, new Position(48.11520, -1.63999));
+    }
+
+    /**
+     * Create a SymbolSitac and add it to the SIG.
+     *
+     * @param color the color
+     * @param shape the shape
+     * @param payload the payload
+     * @param location the location
+     */
+    private void addToSig(Color color, Shape shape, String payload, Position location) {
+        Symbol sym = new Symbol(color, shape, payload);
+        symbols.add(new SymbolSitac(sym, location));
     }
 
     @Override
