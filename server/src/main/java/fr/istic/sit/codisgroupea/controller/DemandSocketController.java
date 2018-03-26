8164ed28cb2d71c2acada01519a813ebf8e8be46
Jordan Maheu <jmaheu1995@gmail.com>
2018-03-26 10:59:29 +0200

    [US-1][#18] DemandSocketController's method declaration done. Javadoc done.

--- server/src/main/java/fr/istic/sit/codisgroupea/controller/DemandSocketController.java
+++ server/src/main/java/fr/istic/sit/codisgroupea/controller/DemandSocketController.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
 
+/**
+ * Controller for demand routes.
+ */
 @Controller
 public class DemandSocketController {
 
@@ -18,46 +21,53 @@ public class DemandSocketController {
     }
 
     /**
-     *
+     * Method to create a unit demand.
      */
-    @MessageMapping()
-    @SendTo({"users/{username}"})
-    public String detailUser(@DestinationVariable("username") final String username, Principal principal, String dataSendByClient) {
+    @MessageMapping("/app/interventions/{id}/units/create")
+    public void createUnit(@DestinationVariable("id") final String id, Principal principal, String dataSendByClient) {
+        String userLogin = principal.getName();
+
+        //Message for the codis
+        simpMessagingTemplate.convertAndSendToUser(userLogin,"/topic/demandes/created","msgToSend");
+
+        //Message for the client
+        simpMessagingTemplate.convertAndSendToUser(userLogin,"/topic/interventions/{id}/units/event","msgToSend");
+    }
+
+    /**
+     * Method to update a unit.
+     */
+    @MessageMapping("/app/interventions/{id}/units/update")
+    @SendTo({"/topic/interventions/{id}/units/event"})
+    public String updateUnit(@DestinationVariable("id") final String id, Principal principal, String dataSendByClient) {
         return "";
     }
 
     /**
-     *
+     * Method to validate a client demand.
      */
-    @MessageMapping("users/{username}/subscribed")
-    public String subscribeUser(Principal principal, String dataSendByClient) {
-        return "";
+    @MessageMapping("/app/demandes/{idUnit}/accept")
+    public void confirmDemand(@DestinationVariable("idUnit") final String idUnit, Principal principal, String dataSendByClient) {
+        String userLogin = principal.getName();
+
+        //Message for the codis
+        simpMessagingTemplate.convertAndSendToUser(userLogin,"/topic/demandes/{id}/accepted","msgToSend");
+
+        //Message for the client
+        simpMessagingTemplate.convertAndSendToUser(userLogin,"/topic/interventions/{id}/units/{id}/accepted","msgToSend");
     }
 
     /**
-     *
+     * Method to deny a client demand.
      */
-    @MessageMapping()
-    @SendTo({"users/{username}/ack/intervention"})
-    public String ackIntervention(@DestinationVariable("username") final String username, Principal principal, String dataSendByClient) {
-        return "";
-    }
+    @MessageMapping("/app/demandes/{idUnit}/deny")
+    public void denyDemand(@DestinationVariable("idUnit") final String idUnit, Principal principal, String dataSendByClient) {
+        String userLogin = principal.getName();
 
-    /**
-     *
-     */
-    @MessageMapping()
-    @SendTo({"users/{username}/ack/demande"})
-    public String ackDemande(@DestinationVariable("username") final String username, Principal principal, String dataSendByClient) {
-        return "";
-    }
+        //Message for the codis
+        simpMessagingTemplate.convertAndSendToUser(userLogin,"/topic/demandes/{id}/denied","msgToSend");
 
-    /**
-     *
-     */
-    @MessageMapping()
-    @SendTo({"users/{username}/ack/symbol"})
-    public String ackSymbol(@DestinationVariable("username") final String username, Principal principal, String dataSendByClient) {
-        return "";
+        //Message for the client
+        simpMessagingTemplate.convertAndSendToUser(userLogin,"/topic/interventions/{id}/units/{id}/denied","msgToSend");
     }
 }
