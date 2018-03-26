3309a33a8061c06695c408cf5af6d9295d10dcc8
Unknown <beaulielloic@gmail.com>
2018-03-26 17:00:24 +0200

    finish authenticationController

--- server/src/main/java/fr/istic/sit/codisgroupea/model/message/InitializeApplicationMessage.java
+++ server/src/main/java/fr/istic/sit/codisgroupea/model/message/InitializeApplicationMessage.java
@@ -9,31 +9,58 @@ import java.util.List;
  */
 public class InitializeApplicationMessage {
 
+    /**
+     * User who ask data
+     */
     private UserMessage user;
+    /**
+     * Type vehicle list
+     */
     private List<VehicleTypeMessage> types;
+    /**
+     * Codes sinister list
+     */
     private List<SinisterCodeMessage> codes;
+    /**
+     * vehicle list
+     */
     private List<VehicleMessage> vehicles;
-    private List<DemmandeMessage> demandes;
+    /**
+     * Demand list
+     */
+    private List<DemandMessage> demandes;
+    /**
+     * ???
+     */
     private List<VehicleColorMapping> vehicle_color_mapping;
 
 
+    /**
+     *
+     * @param usr User who ask data
+     * @param typesList Type vehicle list
+     * @param codesList Codes sinister list
+     * @param vehicleList vehicle list
+     * @param demandList Demand list
+     * @param vehicleColorMapping ???
+     */
     public InitializeApplicationMessage(User usr, List<VehicleTypeMessage> typesList,
                                         List<SinisterCodeMessage> codesList,
                                         List<VehicleMessage> vehicleList,
-                                        List<DemmandeMessage> demandeList,
+                                        List<DemandMessage> demandList,
                                         List<VehicleColorMapping> vehicleColorMapping){
         user = new UserMessage(usr);
         types = typesList;
         codes = codesList;
         vehicles = vehicleList;
-        demandes = demandeList;
+        demandes = demandList;
         vehicle_color_mapping = vehicleColorMapping;
 
     }
 
 
     /**
-     *
+     * utilité ?
      */
     public static class VehicleColorMapping{
         private String code;
@@ -49,11 +76,20 @@ public class InitializeApplicationMessage {
     }
 
     /**
-     * represent a vehicule send to the client
+     * Represent a vehicule send to the client
      */
     public static class VehicleMessage {
+        /**
+         * vehicle label
+         */
         private String label;
+        /**
+         * Type vehicle
+         */
         private String type;
+        /**
+         * status vehicle from the class {@link VehicleStatus}
+         */
         private VehicleStatus status;
 
         /**
@@ -68,10 +104,12 @@ public class InitializeApplicationMessage {
     }
 
     /**
-     * Represent a vehicule type simplify to send to client
+     * Represent a vehicule type simplify to send to the client
      */
     public static class VehicleTypeMessage {
-
+        /**
+         * VehicleType label
+         */
         private String label;
 
         /**
@@ -93,7 +131,13 @@ public class InitializeApplicationMessage {
      */
     public static class UserMessage {
 
+        /**
+         * username of the User
+         */
         private String username;
+        /**
+         * Role of the user
+         */
         private String role;
 
         /**
@@ -110,7 +154,13 @@ public class InitializeApplicationMessage {
      */
     public static class SinisterCodeMessage {
 
+        /**
+         * SinisterCode label
+         */
         private String label;
+        /**
+         * SinisterCode description
+         */
         private String description;
 
         /**
@@ -124,17 +174,31 @@ public class InitializeApplicationMessage {
     }
 
     /**
-     *
+     * Message to know the list of requested vehicle
      */
-    public static class DemmandeMessage {
+    public static class DemandMessage {
 
+        /**
+         * id Demand
+         */
         private int id;
+        /**
+         * Vehicle designed by the demand
+         */
         private VehicleMessage vehicle;
 
+        /**
+         *
+         * @param unit Unit who represent a demand. unit.vehicle. Status is necessary on requested
+         */
+        public DemandMessage(Unit unit){
 
-        public DemmandeMessage(int id, Vehicle vehicle){
-            this.id = id;
-            this.vehicle = new VehicleMessage(vehicle);
+            if (unit.getVehicle().getStatus() != VehicleStatus.REQUESTED){
+                throw new IllegalArgumentException("You created a DemandMessage but unit.vehicle status is not equal to requested");
+            }
+
+            this.id = unit.getId();
+            this.vehicle = new VehicleMessage(unit.getVehicle());
 
         }
     }
--- server/src/main/java/fr/istic/sit/codisgroupea/repository/UnitRepository.java
+++ server/src/main/java/fr/istic/sit/codisgroupea/repository/UnitRepository.java
@@ -2,11 +2,17 @@ package fr.istic.sit.codisgroupea.repository;
 
 import fr.istic.sit.codisgroupea.model.entity.Unit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * SpringData repository for unit entity.
  */
 @Repository
 public interface UnitRepository extends JpaRepository<Unit, Long> {
+
+    @Query("select u from Unit u where u.vehicle.status = REQUESTED")
+    List<Unit> getAllReqestedVehicle();
 }
