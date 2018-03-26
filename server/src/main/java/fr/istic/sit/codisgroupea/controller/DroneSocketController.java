80f8c6075f09f001d8d70007012d07e19ce65bb0
Jordan Maheu <jmaheu1995@gmail.com>
2018-03-26 08:00:29 +0200

    [US-1][#18] Change the different controller and add User Controller

--- server/src/main/java/fr/istic/sit/codisgroupea/controller/DroneSocketController.java
+++ server/src/main/java/fr/istic/sit/codisgroupea/controller/DroneSocketController.java
@@ -1,6 +1,12 @@
 package fr.istic.sit.codisgroupea.controller;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 /**
  * Controller for drone routes
@@ -9,4 +15,19 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class DroneSocketController {
+
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    public DroneSocketController (SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
+    /**
+     *
+     */
+    @MessageMapping()
+    @SendTo({"interventions/{id}/drone/photos/create"})
+    public String createPhotoDrone(@DestinationVariable("id") final String id, Principal principal, String dataSendByClient) {
+        return "";
+    }
 }
--- server/src/main/java/fr/istic/sit/codisgroupea/controller/SymbolSocketController.java
+++ server/src/main/java/fr/istic/sit/codisgroupea/controller/SymbolSocketController.java
@@ -1,7 +1,12 @@
 package fr.istic.sit.codisgroupea.controller;
 
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 /**
  * Controller for symbol routes
@@ -9,5 +14,46 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class SymbolSocketController {
+
     private SimpMessagingTemplate simpMessagingTemplate;
+
+    public SymbolSocketController (SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
+    /**
+     *
+     */
+    @MessageMapping()
+    @SendTo({"interventions/{id}/symbols/event"})
+    public String eventSymbols(@DestinationVariable("id") final String id, Principal principal, String dataSendByClient) {
+        return "";
+    }
+
+    /**
+     *
+     */
+    @MessageMapping()
+    @SendTo({"interventions/{id}/symbols/create"})
+    public String createSymbols(@DestinationVariable("id") final String id, Principal principal, String dataSendByClient) {
+        return "";
+    }
+
+    /**
+     *
+     */
+    @MessageMapping()
+    @SendTo({"interventions/{id}/symbols/{id_symb}/delete"})
+    public String deleteSymbols(@DestinationVariable("id") final String id, @DestinationVariable("id_symb") final String id_symb, Principal principal, String dataSendByClient) {
+        return "";
+    }
+
+    /**
+     *
+     */
+    @MessageMapping()
+    @SendTo({"interventions/{id}/symbols/{id_symb}/update"})
+    public String updateSymbols(@DestinationVariable("id") final String id, @DestinationVariable("id_symb") final String id_symb, Principal principal, String dataSendByClient) {
+        return "";
+    }
 }
