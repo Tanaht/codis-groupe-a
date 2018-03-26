dfdf7704e9608c943a1980cbe8941c727ec7355a
Unknown <christophe.zbirka@wanadoo.fr>
2018-03-26 11:40:59 +0200

    changement image avec clic: ok

--- android/app/src/main/java/ila/fr/codisintervention/Activities/TestFragmentActivity.java
+++ android/app/src/main/java/ila/fr/codisintervention/Activities/TestFragmentActivity.java
@@ -1,16 +1,29 @@
 package ila.fr.codisintervention.Activities;
 
+import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import ila.fr.codisintervention.R;
 
 public class TestFragmentActivity  extends FragmentActivity {
 
+    ImageView ressourceEau;
+
     @Override
     protected void onCreate(Bundle arg0) {
         super.onCreate(arg0);
         setContentView(R.layout.fragment_liste_symboles);
+    }
+
+    public void imageClick(View view){
+        ressourceEau = (ImageView) view.findViewById(R.id.ressource_eau);
+        ressourceEau.setImageResource(R.drawable.ressource_eau_glow);
+
     }
 }
