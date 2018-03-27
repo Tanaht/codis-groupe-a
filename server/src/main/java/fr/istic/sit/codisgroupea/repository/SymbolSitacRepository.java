9d9513c793da7ab0d81e63562012e78b3e66cd77
Kévin Le Bon <kevin.le_bon@yahoo.fr>
2018-03-27 10:57:57 +0200

    [US-1][#18] Add timestamp to photo and a custom request in repository

--- server/src/main/java/fr/istic/sit/codisgroupea/repository/SymbolSitacRepository.java
+++ server/src/main/java/fr/istic/sit/codisgroupea/repository/SymbolSitacRepository.java
@@ -1,9 +1,13 @@
 package fr.istic.sit.codisgroupea.repository;
 
+import fr.istic.sit.codisgroupea.model.entity.Intervention;
 import fr.istic.sit.codisgroupea.model.entity.SymbolSitac;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SymbolSitacRepository extends JpaRepository<SymbolSitac, Long> {
+    List<SymbolSitac> findAllByIntervention(Intervention intervention);
 }
