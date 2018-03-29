3b0b43dfe835b25dae95de0136852c222a464301
Unknown <beaulielloic@gmail.com>
2018-03-29 14:11:45 +0200

    [US-1][#18] change create intervention input to respectnew CI

--- server/src/main/java/fr/istic/sit/codisgroupea/model/message/intervention/CreateInterventionMessage.java
+++ server/src/main/java/fr/istic/sit/codisgroupea/model/message/intervention/CreateInterventionMessage.java
@@ -2,6 +2,9 @@ package fr.istic.sit.codisgroupea.model.message.intervention;
 
 
 //{"address":"ISTIC, Bat 12 D, Allée Henri Poincaré, Rennes","code":"INC","location":{"lat":48.1156746,"lng":-1.640608}}
+
+import java.util.List;
+
 /**
  * The create-intervention message.
  */
@@ -19,6 +22,8 @@ public class CreateInterventionMessage {
      */
     public Position location;
 
+    public List<Unit> units;
+
     /**
      * Instantiates a new create-intervention message.
      *
@@ -32,6 +37,58 @@ public class CreateInterventionMessage {
         this.location = location;
     }
 
+
+    public static class Unit {
+        private Integer id;
+        private Vehicle vehicle;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public Vehicle getVehicle() {
+            return vehicle;
+        }
+
+        public void setVehicle(Vehicle vehicle) {
+            this.vehicle = vehicle;
+        }
+
+
+        public static class Vehicle{
+            private String label;
+            private String type;
+            private String status;
+
+            public String getLabel() {
+                return label;
+            }
+
+            public void setLabel(String label) {
+                this.label = label;
+            }
+
+            public String getType() {
+                return type;
+            }
+
+            public void setType(String type) {
+                this.type = type;
+            }
+
+            public String getStatus() {
+                return status;
+            }
+
+            public void setStatus(String status) {
+                this.status = status;
+            }
+        }
+    }
 
     /**
      * Instantiates a new Create intervention message.
