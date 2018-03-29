6372740bade9ebdac55732326b1342fe4fd587cd
Unknown <alanmarzin@gmail.com>
2018-03-29 16:04:26 +0200

    [US-1][#18] Format code a little

--- android/app/src/main/java/ila/fr/codisintervention/services/model/ModelService.java
+++ android/app/src/main/java/ila/fr/codisintervention/services/model/ModelService.java
@@ -66,137 +66,76 @@ public class ModelService extends Service implements ModelServiceBinder.IMyServi
     }
 
 
-
     //Méthode pour metre à jour le model
-    public void updateTheModel (Intent intent) {
-        if(intent.getAction() == null)
+    public void updateTheModel(Intent intent) {
+        if (intent.getAction() == null)
             return;
 
-        switch (intent.getAction()){
-            //Pour les messages
-            case  WebsocketService.CONNECT_TO_APPLICATION:
+        switch (intent.getAction()) {
+            case WebsocketService.CONNECT_TO_APPLICATION:
                 InitializeApplication initializeApplication = intent.getParcelableExtra(WebsocketService.CONNECT_TO_APPLICATION);
-                Log.d(TAG, "Connect to application with: "+ initializeApplication.getInterventions().size() + " interventions");
+                Log.d(TAG, "Connect to application with: " + initializeApplication.getInterventions().size() + " interventions");
                 Gson gson = new GsonBuilder().create();
-
-                Log.d(TAG, "RetrievedInitializeApplication: " +  gson.toJson(initializeApplication));
-
-                model.setMessageInitialize(initializeApplication);
-
-                Intent initializeApplicationIntent = new Intent(ModelConstants.ACTION_INITIALIZE_APPLICATION);
-
-
-                Log.d(TAG, "Broadcoast Intent: " + initializeApplicationIntent.getAction());
-                LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(initializeApplicationIntent);
-
+                Log.d(TAG, "RetrievedInitializeApplication: " + gson.toJson(initializeApplication));
+                sendToEveryone(-1, ModelConstants.ACTION_INITIALIZE_APPLICATION);
                 break;
-            //Pour le current Intervention
-            case WebsocketService.INTERVENTION_CHOSEN :
+            case WebsocketService.INTERVENTION_CHOSEN:
                 model.setCurrentIntervention(InterventionChosen.createByIntervention(intent.getParcelableExtra("INTERVENTION_CHOSEN")));
                 break;
-            case WebsocketService.DISCONNECT_TO_APPLICATION :
+            case WebsocketService.DISCONNECT_TO_APPLICATION:
                 model = new BigModel();
-                 break;
-            case WebsocketService.INTERVENTION_CREATED :
+                break;
+            case WebsocketService.INTERVENTION_CREATED:
                 Intervention intervention = intent.getParcelableExtra("INTERVENTION_CREATED");
                 model.getMessageInitialize().getInterventions().add(intervention);
-
-
-                Intent intentCreatedIntervention = new Intent(ModelConstants.ACTION_ADD_INTERVENTION);
-                // Adding some data
-                intentCreatedIntervention.putExtra("id",intervention.getId());
-
-                Log.d(TAG, "Broadcoast Intent: " + intentCreatedIntervention.getAction());
-
-                LocalBroadcastManager.getInstance(this).sendBroadcast(intentCreatedIntervention);
+                sendToEveryone(intervention.getId(), ModelConstants.ACTION_ADD_INTERVENTION);
                 break;
-            case WebsocketService.INTERVENTION_CLOSED :
-                int id = intent.getIntExtra(WebsocketService.INTERVENTION_CLOSED,-1);
-                model.getMessageInitialize().setInterventionClosedById(intent.getIntExtra("INTERVENTION_CLOSED",-1));
-                Intent intentClosedIntervention = new Intent(ModelConstants.ACTION_DELETE_INTERVENTION);
-                // Adding some data
-                intentClosedIntervention.putExtra("id",id);
-                LocalBroadcastManager.getInstance(this).sendBroadcast(intentClosedIntervention);
+            case WebsocketService.INTERVENTION_CLOSED:
+                int id = intent.getIntExtra(WebsocketService.INTERVENTION_CLOSED, -1);
+                model.getMessageInitialize().setInterventionClosedById(intent.getIntExtra("INTERVENTION_CLOSED", -1));
+                sendToEveryone(id, ModelConstants.ACTION_DELETE_INTERVENTION);
                 break;
-            case WebsocketService.INTERVENTION_SYMBOL_CREATED :
+            case WebsocketService.INTERVENTION_SYMBOL_CREATED:
                 Symbol symbolCreated = intent.getParcelableExtra(WebsocketService.INTERVENTION_SYMBOL_CREATED);
                 model.getCurrentIntervention().getSymbols().add(symbolCreated);
-
-                Intent intentCreatedSymbol = new Intent(ModelConstants.ACTION_UPDATE_INTERVENTION_CREATE_SYMBOL);
-                // Adding some data
-                intentCreatedSymbol.putExtra("id",symbolCreated.getId());
-
-                Log.d(TAG, "Broadcoast Intent: " + intentCreatedSymbol.getAction());
-
-                LocalBroadcastManager.getInstance(this).sendBroadcast(intentCreatedSymbol);
-
+                sendToEveryone(symbolCreated.getId(), ModelConstants.ACTION_UPDATE_INTERVENTION_CREATE_SYMBOL);
                 break;
-            case WebsocketService.INTERVENTION_SYMBOL_UPDATED :
+            case WebsocketService.INTERVENTION_SYMBOL_UPDATED:
                 Symbol symbolUpdated = intent.getParcelableExtra
                         (WebsocketService.INTERVENTION_SYMBOL_UPDATED);
                 model.getCurrentIntervention()
                         .changeSymbol(symbolUpdated);
-                Intent intentUpdatedSymbol = new Intent(ModelConstants.ACTION_UPDATE_INTERVENTION_UPDATE_SYMBOL);
-                // Adding some data
-                intentUpdatedSymbol.putExtra("id",symbolUpdated.getId());
-
-                Log.d(TAG, "Broadcoast Intent: " + intentUpdatedSymbol.getAction());
-
-                LocalBroadcastManager.getInstance(this).sendBroadcast(intentUpdatedSymbol);
-
+                sendToEveryone(symbolUpdated.getId(), ModelConstants.ACTION_UPDATE_INTERVENTION_UPDATE_SYMBOL);
                 break;
-            case WebsocketService.INTERVENTION_SYMBOL_DELETED :
-                int idSymbol = intent.getIntExtra(WebsocketService.INTERVENTION_SYMBOL_DELETED,-1);
+            case WebsocketService.INTERVENTION_SYMBOL_DELETED:
+                int idSymbol = intent.getIntExtra(WebsocketService.INTERVENTION_SYMBOL_DELETED, -1);
                 model.getCurrentIntervention().deleteSymbolById(idSymbol);
-                Intent intentDeleteSymbol = new Intent(ModelConstants.ACTION_UPDATE_INTERVENTION_DELETE_SYMBOL);
-                // Adding some data
-                intentDeleteSymbol.putExtra("id",idSymbol);
-
-                Log.d(TAG, "Broadcoast Intent: " + intentDeleteSymbol.getAction());
-
-                LocalBroadcastManager.getInstance(this).sendBroadcast(intentDeleteSymbol);
-
+                sendToEveryone(idSymbol, ModelConstants.ACTION_UPDATE_INTERVENTION_DELETE_SYMBOL);
                 break;
-            case WebsocketService.INTERVENTION_UNIT_CREATED :
+            case WebsocketService.INTERVENTION_UNIT_CREATED:
                 Unit unitCreated = intent.getParcelableExtra
                         (WebsocketService.INTERVENTION_UNIT_CREATED);
                 model.getCurrentIntervention().getUnits().add(unitCreated);
-
-                Intent intentCreatedUnit = new Intent(ModelConstants.ACTION_UPDATE_INTERVENTION_CREATE_UTIL);
-                // Adding some data
-                intentCreatedUnit.putExtra("id",unitCreated.getId());
-
-                Log.d(TAG, "Broadcoast Intent: " + intentCreatedUnit.getAction());
-
-                LocalBroadcastManager.getInstance(this).sendBroadcast(intentCreatedUnit);
-
+                sendToEveryone(unitCreated.getId(), ModelConstants.ACTION_UPDATE_INTERVENTION_CREATE_UTIL);
                 break;
-            case WebsocketService.INTERVENTION_UNIT_UPDATED :
-               Unit unitUpdated = intent.getParcelableExtra
-                       (WebsocketService.INTERVENTION_UNIT_UPDATED);
+            case WebsocketService.INTERVENTION_UNIT_UPDATED:
+                Unit unitUpdated = intent.getParcelableExtra
+                        (WebsocketService.INTERVENTION_UNIT_UPDATED);
                 model.getCurrentIntervention().changeUnit(unitUpdated);
-
-                Intent intentUpdatedUnit = new Intent(ModelConstants.ACTION_UPDATE_INTERVENTION_UPDATE_UTIL);
-                // Adding some data
-                intentUpdatedUnit.putExtra("id",unitUpdated.getId());
-
-                Log.d(TAG, "Broadcoast Intent: " + intentUpdatedUnit.getAction());
-
-                LocalBroadcastManager.getInstance(this).sendBroadcast(intentUpdatedUnit);
-
+                sendToEveryone(unitUpdated.getId(), ModelConstants.ACTION_UPDATE_INTERVENTION_UPDATE_UTIL);
                 break;
-            case WebsocketService.DEMANDE_ACCEPTED :
+            case WebsocketService.DEMANDE_ACCEPTED:
                 break;
-            case WebsocketService.DEMANDE_DENIED :
+            case WebsocketService.DEMANDE_DENIED:
                 break;
-            case WebsocketService.DEMANDE_CREATED :
+            case WebsocketService.DEMANDE_CREATED:
                 break;
             case WebsocketService.DRONE_PING:
                 break;
-            case WebsocketService.DRONE_PHOTO :
+            case WebsocketService.DRONE_PHOTO:
                 break;
             //Si l'action est mal définit
-            default :
+            default:
                 Log.e("Erreur", "Erreur d'action non reconnu pour la mise à jour du model");
         }
     }
@@ -238,5 +177,15 @@ public class ModelService extends Service implements ModelServiceBinder.IMyServi
     @Override
     public Unit getUnit(int id) {
         return model.getCurrentIntervention().getUnitById(id);
+    }
+
+    public void sendToEveryone(int id, String nameAction){
+        Intent intent = new Intent(nameAction);
+        if(id!=-1) {
+            // Adding some data
+            intent.putExtra("id", id);
+        }
+        Log.d(TAG, "Broadcoast Intent: " + intent.getAction());
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
     }
 }
