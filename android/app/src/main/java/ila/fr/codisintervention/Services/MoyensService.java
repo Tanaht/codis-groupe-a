2f4e9d82fc03b0bd416e34613564ad2e5cf9264c
masoumiaa <aminesoumiaa@gmail.com>
2018-03-30 09:10:52 +0200

    [FIX] : INIT VEHICLS CHECKBOX STATUS TO FALSE

--- android/app/src/main/java/ila/fr/codisintervention/Services/MoyensService.java
+++ android/app/src/main/java/ila/fr/codisintervention/Services/MoyensService.java
@@ -13,10 +13,10 @@ public class MoyensService {
     public ArrayList<Moyen> getMoyensDispo(){
         ArrayList<Moyen> MoyenDispoList = new ArrayList<Moyen>();
         MoyenDispoList.add(new Moyen("VLCG-13XP201","VLCG",false));
-        MoyenDispoList.add(new Moyen("VLCG-13XP103","VLCG",true));
+        MoyenDispoList.add(new Moyen("VLCG-13XP103","VLCG",false));
         MoyenDispoList.add(new Moyen("VLCG-13XP205","VLCG",false));
-        MoyenDispoList.add(new Moyen("VSAV-12VS32","VSAV",true));
-        MoyenDispoList.add(new Moyen("VSAV-12VS20","VSAV",true));
+        MoyenDispoList.add(new Moyen("VSAV-12VS32","VSAV",false));
+        MoyenDispoList.add(new Moyen("VSAV-12VS20","VSAV",false));
         MoyenDispoList.add(new Moyen("FPT-F21","FPT",false));
         MoyenDispoList.add(new Moyen("FPT-F14","FPT",false));
         return MoyenDispoList;
