82ec8f847add84d276e7b05dd81593d0f248a9c4
Tanaky <charp.antoine@gmail.com>
2018-04-14 18:26:03 +0200

    [TICKET][#89] Javadoc of DrawingFragment completed

--- android/app/src/main/java/ila/fr/codisintervention/fragments/drawingFragment.java
+++ android/app/src/main/java/ila/fr/codisintervention/fragments/drawingFragment.java
@@ -11,71 +11,35 @@ import android.view.ViewGroup;
 import ila.fr.codisintervention.R;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link drawingFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link drawingFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * DrawingFragment
+ * This fragment is used for nothing for now
  */
-public class drawingFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+public class DrawingFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
 
-    public drawingFragment() {
+    /**
+     * Empty Public Constructor
+     */
+    public DrawingFragment() {
         // Required empty public constructor
     }
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment drawingFragment.
+     * @return A new instance of fragment DrawingFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static drawingFragment newInstance(String param1, String param2) {
-        drawingFragment fragment = new drawingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public static DrawingFragment newInstance() {
+        return new DrawingFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
-        return inflater.inflate(R.layout.fragment_dessin, container, false);
+        return inflater.inflate(R.layout.fragment_drawing, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
+    @SuppressWarnings("squid:S00112")
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -93,19 +57,10 @@ public class drawingFragment extends Fragment {
         mListener = null;
     }
 
-
     /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
+     * Interface used to define event from this fragment to whomever who subscribe to it.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 
