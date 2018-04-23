1bdb27bcd27021b98f81b408ed908c3e2ded82ad
Tanaky <charp.antoine@gmail.com>
2018-04-23 11:06:22 +0200

    [TICKET][#90] FInish mutualization of Service Subscriptions

--- android/app/src/main/java/ila/fr/codisintervention/activities/SecondMainActivity.java
+++ android/app/src/main/java/ila/fr/codisintervention/activities/SecondMainActivity.java
@@ -7,12 +7,29 @@ import android.util.Log;
 import android.view.View;
 
 import ila.fr.codisintervention.R;
+import ila.fr.codisintervention.binders.ModelServiceBinder;
+import ila.fr.codisintervention.binders.WebSocketServiceBinder;
+import ila.fr.codisintervention.services.ModelServiceAware;
+import ila.fr.codisintervention.services.WebSocketServiceAware;
 
+/**
+ * Stub to be suppressed as soon as possible
+ *
+ * Exemple activity to demonstrate how to subscribe to service
+ * TODO: Suppress when unused
+ */
 @SuppressWarnings("squid:MaximumInheritanceDepth")
-public class SecondMainActivity extends AppCompatActivity {
+public class SecondMainActivity extends AppCompatActivity implements WebSocketServiceAware, ModelServiceAware{
     private static final String TAG = "SecondMainActivity";
 
+    /**
+     * Service connection of the service subscribed
+     */
     private ServiceConnection webSocketServiceConnection;
+
+    /**
+     * Service connection of the service subscribed
+     */
     private ServiceConnection modelServiceConnection;
 
     @Override
@@ -22,37 +39,37 @@ public class SecondMainActivity extends AppCompatActivity {
 
         //Here we call the aware interface to perform the binding at our place, we gain an instance of ServiceConnection
         Log.d(TAG, "Bind to services");
-        //modelServiceConnection = bindModelService();
-        //webSocketServiceConnection = bindWebSocketService();
+        modelServiceConnection = bindModelService();
+        webSocketServiceConnection = bindWebSocketService();
     }
 
-//    @Override
-//    public void onModelServiceConnected() {
-//        Log.i(TAG, "ModelServiceConnected");
-//    }
-//
-//    @Override
-//    public void setModelService(ModelServiceBinder.IMyServiceMethod modelService) {
-//        Log.d(TAG, "Here we gain an instance of modelService: " + modelService);
-//    }
-//
-//    @Override
-//    public void onWebSocketServiceConnected() {
-//        Log.d(TAG, "WebSocketServiceConnected");
-//    }
-//
-//    @Override
-//    public void setWebSocketService(WebSocketServiceBinder.IMyServiceMethod webSocketService) {
-//        Log.d(TAG, "Here we gain an instance of webSocketService: " + webSocketService);
-//    }
+    @Override
+    public void onModelServiceConnected() {
+        Log.i(TAG, "ModelServiceConnected");
+    }
+
+    @Override
+    public void setModelService(ModelServiceBinder.IMyServiceMethod modelService) {
+        Log.d(TAG, "Here we gain an instance of modelService: " + modelService);
+    }
+
+    @Override
+    public void onWebSocketServiceConnected() {
+        Log.d(TAG, "WebSocketServiceConnected");
+    }
+
+    @Override
+    public void setWebSocketService(WebSocketServiceBinder.IMyServiceMethod webSocketService) {
+        Log.d(TAG, "Here we gain an instance of webSocketService: " + webSocketService);
+    }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
         //We unbind from the registered services
-//        unbindModelService(modelServiceConnection);
-//        unbindWebSocketService(webSocketServiceConnection);
+        unbindModelService(modelServiceConnection);
+        unbindWebSocketService(webSocketServiceConnection);
     }
 
     public void onClick(View view) {
