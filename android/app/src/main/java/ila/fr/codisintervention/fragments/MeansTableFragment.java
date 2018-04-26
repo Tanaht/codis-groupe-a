deeb63d87679f15eba492df0d84239852317af6d
Unknown <christophe.zbirka@wanadoo.fr>
2018-04-26 08:44:05 +0200

    [#91] very beautyfull table

--- android/app/src/main/java/ila/fr/codisintervention/fragments/MeansTableFragment.java
+++ android/app/src/main/java/ila/fr/codisintervention/fragments/MeansTableFragment.java
@@ -1,7 +1,6 @@
 package ila.fr.codisintervention.fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -16,9 +15,14 @@ import android.widget.TextView;
 
 import ila.fr.codisintervention.R;
 
+/**
+ * The type Means table fragment.
+ */
 public class MeansTableFragment extends Fragment {
+    /**
+     * The Title.
+     */
     TextView title;
-    private TableLayout meansTable;
 
     @Nullable
     @Override
@@ -30,28 +34,37 @@ public class MeansTableFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         title = (TextView) view.findViewById(R.id.title);
-
-        title.setText("Je suis un fragment");
+        title.setText(getString(R.string.title_means_table));
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        TableLayout meansTable;
         meansTable = (TableLayout) getActivity().findViewById(R.id.meansTable);
 
-        String[] titles = getResources().getStringArray(R.array.titles);
+        String[] headers = {
+                getResources().getString(R.string.header_1),
+                getResources().getString(R.string.header_2),
+                getResources().getString(R.string.header_3),
+                getResources().getString(R.string.header_4),
+                getResources().getString(R.string.header_5),
+                getResources().getString(R.string.header_6)};
 
         TableRow tableRow = new TableRow(getActivity());
         meansTable.addView(tableRow, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
         meansTable.setBackgroundColor(getResources().getColor(R.color.grey));
 
-        tableRow.setLayoutParams(new LayoutParams(titles.length));
+        tableRow.setLayoutParams(new LayoutParams(headers.length));
 
         int i = 0;
-        for (String player : titles) {
-            TextView text = createTextView(false , i == titles.length - 1);
-            text.setText(player);
+        for (String header : headers) {
+            TextView text = createTextView(false , i == headers.length - 1,0);
+            text.setText(header);
+            text.setBackgroundResource(R.color.colorPrimary);
+            text.setTextSize(20);
+            text.setTypeface(text.getTypeface(), Typeface.BOLD);
             text.setGravity(Gravity.CENTER);
             tableRow.addView(text, i++);
         }
@@ -61,9 +74,10 @@ public class MeansTableFragment extends Fragment {
             tableRow = new TableRow(getActivity());
             meansTable.addView(tableRow, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
             i = 0;
-            for (String player : titles) {
-                TextView text = createTextView(j==9, i == titles.length - 1);
+            for (String header : headers) {
+                TextView text = createTextView(j==9, i == headers.length - 1, j);
                 text.setText("123");
+
                 tableRow.addView(text, i++);
                 text.setGravity(Gravity.RIGHT);
             }
@@ -71,7 +85,7 @@ public class MeansTableFragment extends Fragment {
 
     }
 
-    private TextView createTextView(boolean endline, boolean endcolumn){
+    private TextView createTextView(boolean endline, boolean endcolumn, int line){
         TextView text = new TextView(getActivity(), null, R.style.meansTabHeaderCol);
         int bottom = endline ? 1 : 0;
         int right = endcolumn ? 1 :0;
@@ -79,7 +93,13 @@ public class MeansTableFragment extends Fragment {
         params.setMargins(1, 1, right, bottom);
         text.setLayoutParams(params);
         text.setPadding(4, 4, 10, 4);
-        text.setBackgroundColor(getResources().getColor(R.color.white));
+        if(line%2 == 0){
+            text.setBackgroundColor(getResources().getColor(R.color.white));
+        }
+        else {
+            text.setBackgroundColor(getResources().getColor(R.color.colorPrimaryLight));
+        }
+
         return text;
     }
 
