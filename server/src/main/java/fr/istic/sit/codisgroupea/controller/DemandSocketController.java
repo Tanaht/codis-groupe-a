0a17800978a1480974ab4e5f2ee11dc3de275e68
Tanaky <charp.antoine@gmail.com>
2018-04-26 13:43:13 +0200

    [US-6][#51]Move the method UnitUpdate from DemandController into UnitController (to make the both Controller easier to read

--- server/src/main/java/fr/istic/sit/codisgroupea/controller/DemandSocketController.java
+++ server/src/main/java/fr/istic/sit/codisgroupea/controller/DemandSocketController.java
@@ -5,28 +5,24 @@ import fr.istic.sit.codisgroupea.config.RoutesConfig;
 import fr.istic.sit.codisgroupea.exception.InvalidMessageException;
 import fr.istic.sit.codisgroupea.factory.UnitFactory;
 import fr.istic.sit.codisgroupea.model.entity.Intervention;
-import fr.istic.sit.codisgroupea.model.entity.Symbol;
 import fr.istic.sit.codisgroupea.model.entity.Unit;
-import fr.istic.sit.codisgroupea.model.entity.Vehicle;
-import fr.istic.sit.codisgroupea.model.message.ListUnitMessage;
 import fr.istic.sit.codisgroupea.model.message.UnitMessage;
 import fr.istic.sit.codisgroupea.model.message.demand.CreateUnitMessage;
 import fr.istic.sit.codisgroupea.model.message.demand.UnitCreatedMessage;
 import fr.istic.sit.codisgroupea.model.message.receive.ConfirmDemandVehicleMessage;
 import fr.istic.sit.codisgroupea.model.message.send.DemandesCreatedMessage;
-import fr.istic.sit.codisgroupea.repository.*;
+import fr.istic.sit.codisgroupea.repository.InterventionRepository;
+import fr.istic.sit.codisgroupea.repository.SymbolRepository;
+import fr.istic.sit.codisgroupea.repository.UnitRepository;
+import fr.istic.sit.codisgroupea.repository.VehicleRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -111,75 +107,6 @@ public class DemandSocketController {
             logger.error(e.getMessage());
             e.printStackTrace();
         }
-    }
-
-    /**
-     * Method to update a unit.
-     *
-     * @param idInterventions  the id
-     * @param principal        the principal
-     * @param dataSendByClient the data sent by client
-     * @return the string
-     */
-    @MessageMapping(RoutesConfig.UPDATE_UNIT_CLIENT)
-    @SendTo({RoutesConfig.UPDATE_UNIT_SERVER})
-    public ListUnitMessage updateUnit(@DestinationVariable("id") final int idInterventions, Principal principal, List<UnitMessage> dataSendByClient) {
-        Gson jason = new Gson();
-        logger.trace("{} --> data receive {}", RoutesConfig.UPDATE_UNIT_CLIENT, jason.toJson(dataSendByClient));
-
-        Optional<Intervention> intervention = interventionRepository.findById(idInterventions);
-
-        if (!intervention.isPresent()){
-            logger.error("Intervention with id {} doesn't exist", idInterventions);
-        }
-
-        List<UnitMessage> listUnitUpdated = new ArrayList<>();
-
-        for (UnitMessage unitMessageFromCLient : dataSendByClient){
-            Optional<Unit> unitInBdd = unitRepository.findById(unitMessageFromCLient.getId());
-            Optional<Vehicle> vehicle = vehicleRepository.findVehicleByLabel(unitMessageFromCLient.getVehicule().getLabel());
-            Optional<Symbol> symb = symbolRepository
-                    .findSymbolByColorAndShape(unitMessageFromCLient.getSymbolUnitMessage().getColor(),
-                            unitMessageFromCLient.getSymbolUnitMessage().getShape());
-
-            if(!unitInBdd.isPresent()){
-                logger.error("Unit with id {} doesn't exist in bdd", unitMessageFromCLient.getId());
-            }
-
-            if(!vehicle.isPresent()){
-                logger.error("Vehicle with label " + unitMessageFromCLient.getVehicule().getLabel() +
-                        " doesn't exist in bdd");
-            }
-
-            if(!symb.isPresent()){
-                logger.error("sym with color " + unitMessageFromCLient.getSymbolUnitMessage().getColor() +
-                        " and shape " + unitMessageFromCLient.getSymbolUnitMessage().getShape() + "doesn't exist");
-            }
-
-
-            unitInBdd.get().getSymbolSitac().setSymbol(symb.get());
-            unitInBdd.get().getSymbolSitac().setLocation(unitMessageFromCLient
-                    .getSymbolUnitMessage().getLocation().toPositionEntity());
-
-//TODO:            vehicle.get().setStatus(unitMessageFromCLient.getVehicule().getStatus());
-
-            unitInBdd.get().setVehicle(vehicle.get());
-            unitInBdd.get().setMoving(unitMessageFromCLient.isMoving());
-
-            unitInBdd.get().setAcceptDate(new Timestamp(unitMessageFromCLient.getDate_accepted()));
-            unitInBdd.get().setRequestDate(new Timestamp(unitMessageFromCLient.getDate_granted()));
-
-            unitRepository.save(unitInBdd.get());
-            UnitMessage unitUpdated = new UnitMessage(unitInBdd.get());
-
-            listUnitUpdated.add(unitUpdated);
-        }
-
-        //FIXME: Ask question to beaulieu: I have difficulty to understand the reason of the object return
-        ListUnitMessage toReturn = new ListUnitMessage("UPDATE", listUnitUpdated);
-        String json = jason.toJson(toReturn);
-        logger.trace("{} --> data send {}", RoutesConfig.UPDATE_UNIT_SERVER, json);
-        return toReturn;
     }
 
     /**
--- server/src/main/java/fr/istic/sit/codisgroupea/controller/UnitSocketController.java
+++ server/src/main/java/fr/istic/sit/codisgroupea/controller/UnitSocketController.java
@@ -1,10 +1,28 @@
 package fr.istic.sit.codisgroupea.controller;
 
+import com.google.gson.Gson;
+import fr.istic.sit.codisgroupea.config.RoutesConfig;
+import fr.istic.sit.codisgroupea.model.entity.Intervention;
+import fr.istic.sit.codisgroupea.model.entity.Symbol;
+import fr.istic.sit.codisgroupea.model.entity.Unit;
+import fr.istic.sit.codisgroupea.model.entity.Vehicle;
+import fr.istic.sit.codisgroupea.model.message.ListUnitMessage;
+import fr.istic.sit.codisgroupea.model.message.UnitMessage;
 import fr.istic.sit.codisgroupea.repository.InterventionRepository;
 import fr.istic.sit.codisgroupea.repository.UnitRepository;
+import fr.istic.sit.codisgroupea.repository.VehicleRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * WebSocket Controller intended to manage the states of the units
@@ -22,6 +40,8 @@ public class UnitSocketController {
     /** {@link UnitRepository instance to request the database */
     private UnitRepository unitRepository;
 
+    /** {@link VehicleRepository} instance from the DI */
+    private VehicleRepository vehicleRepository;
 
     /**
      * Instantiates a new Unit socket controller.
@@ -29,8 +49,78 @@ public class UnitSocketController {
      * @param interventionRepository the intervention repository
      * @param unitRepository         the unit repository
      */
-    public UnitSocketController(InterventionRepository interventionRepository, UnitRepository unitRepository) {
+    public UnitSocketController(InterventionRepository interventionRepository, UnitRepository unitRepository, VehicleRepository vehicleRepository) {
         this.interventionRepository = interventionRepository;
         this.unitRepository = unitRepository;
+        this.vehicleRepository = vehicleRepository;
+    }
+
+    /**
+     * Method to update a unit.
+     *
+     * @param idInterventions  the id
+     * @param principal        the principal
+     * @param dataSendByClient the data sent by client
+     * @return the string
+     */
+    @MessageMapping(RoutesConfig.UPDATE_UNIT_CLIENT)
+    @SendTo({RoutesConfig.UPDATE_UNIT_SERVER})
+    public ListUnitMessage updateUnit(@DestinationVariable("id") final int idInterventions, Principal principal, List<UnitMessage> dataSendByClient) {
+        Gson jason = new Gson();
+        logger.trace("{} --> data receive {}", RoutesConfig.UPDATE_UNIT_CLIENT, jason.toJson(dataSendByClient));
+
+        Optional<Intervention> intervention = interventionRepository.findById(idInterventions);
+
+        if (!intervention.isPresent()){
+            logger.error("Intervention with id {} doesn't exist", idInterventions);
+        }
+
+        List<UnitMessage> listUnitUpdated = new ArrayList<>();
+
+        for (UnitMessage unitMessageFromCLient : dataSendByClient){
+            Optional<Unit> unitInBdd = unitRepository.findById(unitMessageFromCLient.getId());
+            Optional<Vehicle> vehicle = vehicleRepository.findVehicleByLabel(unitMessageFromCLient.getVehicule().getLabel());
+            Optional<Symbol> symb = symbolRepository
+                    .findSymbolByColorAndShape(unitMessageFromCLient.getSymbolUnitMessage().getColor(),
+                            unitMessageFromCLient.getSymbolUnitMessage().getShape());
+
+            if(!unitInBdd.isPresent()){
+                logger.error("Unit with id {} doesn't exist in bdd", unitMessageFromCLient.getId());
+            }
+
+            if(!vehicle.isPresent()){
+                logger.error("Vehicle with label " + unitMessageFromCLient.getVehicule().getLabel() +
+                        " doesn't exist in bdd");
+            }
+
+            if(!symb.isPresent()){
+                logger.error("sym with color " + unitMessageFromCLient.getSymbolUnitMessage().getColor() +
+                        " and shape " + unitMessageFromCLient.getSymbolUnitMessage().getShape() + "doesn't exist");
+            }
+
+
+            unitInBdd.get().getSymbolSitac().setSymbol(symb.get());
+            unitInBdd.get().getSymbolSitac().setLocation(unitMessageFromCLient
+                    .getSymbolUnitMessage().getLocation().toPositionEntity());
+
+//TODO:            vehicle.get().setStatus(unitMessageFromCLient.getVehicule().getStatus());
+
+            unitInBdd.get().setVehicle(vehicle.get());
+            unitInBdd.get().setMoving(unitMessageFromCLient.isMoving());
+
+            unitInBdd.get().setAcceptDate(new Timestamp(unitMessageFromCLient.getDate_accepted()));
+            unitInBdd.get().setRequestDate(new Timestamp(unitMessageFromCLient.getDate_granted()));
+
+            unitRepository.save(unitInBdd.get());
+            UnitMessage unitUpdated = new UnitMessage(unitInBdd.get());
+
+            listUnitUpdated.add(unitUpdated);
+        }
+
+        //FIXME: Ask question to beaulieu: I have difficulty to understand the reason of the object return
+        ListUnitMessage toReturn = new ListUnitMessage("UPDATE", listUnitUpdated);
+        String json = jason.toJson(toReturn);
+        logger.trace("{} --> data send {}", RoutesConfig.UPDATE_UNIT_SERVER, json);
+        return toReturn;
     }
 }
