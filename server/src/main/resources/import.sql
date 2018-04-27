f058ca8c9bb674de33f2f590ca9d35365ca0a943
Tanaky <charp.antoine@gmail.com>
2018-04-27 09:03:18 +0200

    [US-6][#51] Deny demand worked and update the db correctly

--- server/src/main/resources/import.sql
+++ server/src/main/resources/import.sql
@@ -27,14 +27,9 @@ INSERT INTO vehicle_type VALUES (3, 'VLCG');
 
 
 -- Insert Vehicle
-INSERT INTO vehicle VALUES (1, 'vehicule 1', 2, 1);
-INSERT INTO vehicle VALUES (2, 'vehicule 2', 2, 1);
-
-INSERT INTO vehicle VALUES (3, 'vehicule 3', 2, 2);
-INSERT INTO vehicle VALUES (4, 'vehicule 4', 2, 2);
-
-INSERT INTO vehicle VALUES (5, 'vehicule 5', 2, 3);
-INSERT INTO vehicle VALUES (6, 'vehicule 6', 2, 3);
+INSERT INTO vehicle VALUES (1, 'vehicule 1', 1);
+INSERT INTO vehicle VALUES (2, 'vehicule 2', 2);
+INSERT INTO vehicle VALUES (3, 'vehicule 3', 3);
 
 -- Insert Position
 INSERT INTO position VALUES (1, 48.116487, -1.647416);
