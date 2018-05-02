771bf97879760d779bda784b59f785ecb93d894f
Kévin Le Bon <kevin.le_bon@yahoo.fr>
2018-05-02 16:46:24 +0200

    Refactor the unit controller

--- server/src/main/java/fr/istic/sit/codisgroupea/controller/UnitSocketController.java
+++ server/src/main/java/fr/istic/sit/codisgroupea/controller/UnitSocketController.java
@@ -9,19 +9,16 @@ import fr.istic.sit.codisgroupea.model.entity.Intervention;
 import fr.istic.sit.codisgroupea.model.entity.Unit;
 import fr.istic.sit.codisgroupea.model.message.ListUnitMessage;
 import fr.istic.sit.codisgroupea.model.message.UnitMessage;
-import fr.istic.sit.codisgroupea.repository.InterventionRepository;
-import fr.istic.sit.codisgroupea.repository.SymbolRepository;
 import fr.istic.sit.codisgroupea.repository.UnitRepository;
-import fr.istic.sit.codisgroupea.repository.VehicleRepository;
 import fr.istic.sit.codisgroupea.service.UnitFactory;
+import lombok.val;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-import javax.persistence.PersistenceException;
-import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 import java.util.*;
 
@@ -31,44 +28,37 @@ import java.util.*;
 @Controller
 public class UnitSocketController {
 
+    /**
+     * The logger
+     */
+    private static final Logger logger = LogManager.getLogger();
 
-    /** The logger */
-    private static final org.apache.logging.log4j.Logger logger = LogManager.getLogger();
-
-    /** {@link InterventionRepository} instance */
-    private InterventionRepository interventionRepository;
-
-    /** {@link UnitRepository instance to request the database */
+    /**
+     * {@link UnitRepository instance to request the database
+     */
     private UnitRepository unitRepository;
 
-    /** {@link VehicleRepository} instance from the DI */
-    private VehicleRepository vehicleRepository;
-
-    /** {@link SymbolRepository} instance */
-    private SymbolRepository symbolRepository;
-
-    /** {@link UnitFactory } instance */
+    /**
+     * {@link UnitFactory } instance
+     */
     private UnitFactory unitFactory;
 
-    /** {@link Validator} instance */
+    /**
+     * {@link Validator} instance
+     */
     private Validator validator;
 
     /**
      * Instantiates a new Unit socket controller.
      *
-     * @param interventionRepository the intervention repository
-     * @param unitRepository         the unit repository
+     * @param unitRepository the unit repository
+     * @param unitFactory    the unit factory
+     * @param validator      the validator
      */
-    public UnitSocketController(InterventionRepository interventionRepository,
-                                UnitRepository unitRepository,
-                                VehicleRepository vehicleRepository,
-                                SymbolRepository symbolRepository,
+    public UnitSocketController(UnitRepository unitRepository,
                                 UnitFactory unitFactory,
                                 Validator validator) {
-        this.interventionRepository = interventionRepository;
         this.unitRepository = unitRepository;
-        this.vehicleRepository = vehicleRepository;
-        this.symbolRepository = symbolRepository;
         this.unitFactory = unitFactory;
         this.validator = validator;
     }
@@ -76,63 +66,36 @@ public class UnitSocketController {
     /**
      * Method to update a unit.
      *
-     * @param idIntervention  the id
-     * @param dataSendByClient the data sent by client
+     * @param idIntervention   the id
+     * @param dataSentByClient the data sent by client
      * @return the string
      */
     @MessageMapping(RoutesConfig.UPDATE_UNIT_CLIENT)
     @SendTo({RoutesConfig.UPDATE_UNIT_SERVER})
-    public ListUnitMessage updateUnit(@DestinationVariable("id") final int idIntervention, List<UnitMessage> dataSendByClient) {
+    public ListUnitMessage updateUnit(
+            @DestinationVariable("id") final int idIntervention,
+            List<UnitMessage> dataSentByClient) {
         Gson jason = new GsonBuilder().create();
-        logger.trace("{} --> data received {}", RoutesConfig.UPDATE_UNIT_CLIENT, jason.toJson(dataSendByClient));
-
-        Optional<Intervention> optionalIntervention = interventionRepository.findById(idIntervention);
-
-        if(!optionalIntervention.isPresent()) {
-            throw new PersistenceException("Unable to find intervention with ID " + idIntervention);
-        }
-
-        Intervention intervention = optionalIntervention.get();
+        logger.trace("{} --> data received {}",
+                RoutesConfig.UPDATE_UNIT_CLIENT,
+                jason.toJson(dataSentByClient));
 
         try {
+            checkAccessViolations(dataSentByClient);
 
-            Set<ConstraintViolation<List<UnitMessage>>> violations = validator.validate(dataSendByClient, Message.IdAware.class);
-
-            if(!violations.isEmpty())
-                throw new InvalidMessageException(UnitMessage.class, violations.toString());
-
+            // Transform the messages into valid units for persistence.
             List<Unit> toBePersisted = new ArrayList<>();
-
-            dataSendByClient.forEach(unitMessage -> {
-                Optional<Unit> optionalUnit = unitRepository.findOneById(unitMessage.getId());
-
-                if(!optionalUnit.isPresent()) {
-                    logger.error("Cannot retrieve unit with ID {}", unitMessage.getId());
-                    return;
-                }
-                Unit unit = optionalUnit.get();
-                if(toBePersisted.contains(unit)) {
-                    logger.warn("Cannot update the same unit twice in a single message");
-                    return;
-                }
-
-                try {
-                    unitFactory.updateUnit(unit, unitMessage);
-                    toBePersisted.add(unit);
-                } catch (InvalidMessageException e) {
-                    logger.error(e.getMessage());
-                    e.printStackTrace();
-                }
-            });
+            for(val unitMessage : dataSentByClient) {
+                persistUnit(toBePersisted, unitMessage);
+            }
 
             unitRepository.saveAll(toBePersisted);
 
+            // Transform the previously built units into messages.
             List<UnitMessage> unitMessages = new ArrayList<>();
-
-            toBePersisted.forEach(unit -> {
+            for(val unit : toBePersisted) {
                 unitMessages.add(new UnitMessage(unit));
-            });
-
+            }
 
             return new ListUnitMessage("UPDATE", unitMessages);
         } catch (InvalidMessageException e) {
@@ -140,5 +103,54 @@ public class UnitSocketController {
             e.printStackTrace();
         }
         return new ListUnitMessage("UPDATE", Collections.emptyList());
+    }
+
+    /**
+     * Add the unit described by the message into the persistence list.
+     *
+     * @param persist     the persistence list
+     * @param unitMessage the unit message
+     */
+    private void persistUnit(List<Unit> persist, UnitMessage unitMessage) {
+        val optionalUnit = unitRepository.findOneById(
+                unitMessage.getId());
+
+        if (!optionalUnit.isPresent()) {
+            val msg = "Cannot retrieve unit with ID {}";
+            logger.error(msg, unitMessage.getId());
+            return;
+        }
+
+        Unit unit = optionalUnit.get();
+        if (persist.contains(unit)) {
+            val msg = "Cannot update the same unit twice in a single message";
+            logger.warn(msg);
+            return;
+        }
+
+        try {
+            unitFactory.updateUnit(unit, unitMessage);
+            persist.add(unit);
+        } catch (InvalidMessageException e) {
+            logger.error(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Check if no access has been violated.
+     *
+     * @param dataSentByClient         the data sent by the client
+     * @throws InvalidMessageException thrown in case of access violation
+     */
+    private void checkAccessViolations(List<UnitMessage> dataSentByClient)
+            throws InvalidMessageException {
+        val violations = validator.validate(dataSentByClient,
+                Message.IdAware.class);
+
+        if (!violations.isEmpty()) {
+            val msg = violations.toString();
+            throw new InvalidMessageException(UnitMessage.class, msg);
+        }
     }
 }
