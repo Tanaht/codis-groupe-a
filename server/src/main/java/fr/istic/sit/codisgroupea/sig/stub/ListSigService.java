fa3fb67426a6c5c837941b5b1736f696a3fe1846
Kévin Le Bon <kevin.le_bon@yahoo.fr>
2018-05-02 17:36:28 +0200

    Refactor the list SIG service and parts of the intervention controller

--- server/src/main/java/fr/istic/sit/codisgroupea/sig/stub/ListSigService.java
+++ server/src/main/java/fr/istic/sit/codisgroupea/sig/stub/ListSigService.java
@@ -2,11 +2,13 @@ package fr.istic.sit.codisgroupea.sig.stub;
 
 import fr.istic.sit.codisgroupea.model.entity.*;
 import fr.istic.sit.codisgroupea.sig.SigRepository;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import static java.lang.Math.*;
 
 /**
@@ -25,44 +27,53 @@ public class ListSigService implements SigRepository<SymbolSitac> {
     public ListSigService() {
         symbols = new ArrayList<>();
 
-        addToSig(Color.BLUE, Shape.WATERRESOURCE, null, new Position(48.11540, -1.63813));
-        addToSig(Color.ORANGE, Shape.DANGERDOWN, null, new Position(48.11520, -1.63999));
+        addToSig(Color.BLUE, Shape.WATERRESOURCE, null,
+                new Position(48.11540, -1.63813));
+        addToSig(Color.ORANGE, Shape.DANGERDOWN, null,
+                new Position(48.11520, -1.63999));
     }
 
     /**
      * Create a SymbolSitac and add it to the SIG.
      *
-     * @param color the color
-     * @param shape the shape
-     * @param payload the payload
+     * @param color    the color
+     * @param shape    the shape
+     * @param payload  the payload
      * @param location the location
      */
-    private void addToSig(Color color, Shape shape, Payload payload, Position location) {
+    private void addToSig(Color color,
+                          Shape shape,
+                          Payload payload,
+                          Position location) {
         Symbol sym = new Symbol(color, shape);
         symbols.add(new SymbolSitac(null, sym, location, payload));
     }
 
     @Override
-    public SymbolSitac createSymbolFromSigEntry(Intervention intervention, SymbolSitac entry) {
+    public SymbolSitac createSymbolFromSigEntry(Intervention intervention,
+                                                SymbolSitac entry) {
         entry.setIntervention(intervention);
         return entry;
     }
 
     @Override
-    public List<SymbolSitac> getSymbolsInTheIntervention(Intervention intervention){
+    public List<SymbolSitac> getInterventionSymbols(Intervention intervention) {
         List<SymbolSitac> listSymbolBouchon = new ArrayList<>();
 
 
         List<SymbolSitac> listSymbAssociateWithIntervention = new ArrayList<>();
-        for (SymbolSitac symbolSitac : listSymbolBouchon){
-            if(getMeters(intervention.getPosition(), symbolSitac.getLocation()) <= sizeFromTheInterventionCenter){
-                SymbolSitac copySymbolSitac = new SymbolSitac();
+        for (SymbolSitac symbolSitac : listSymbolBouchon) {
+            val distance = getMeters(
+                    intervention.getPosition(),
+                    symbolSitac.getLocation());
+
+            if (distance <= sizeFromTheInterventionCenter) {
+                val copySymbolSitac = new SymbolSitac();
 
                 copySymbolSitac.setLocation(symbolSitac.getLocation());
                 copySymbolSitac.setSymbol(symbolSitac.getSymbol());
                 copySymbolSitac.setId(symbolSitac.getId());
                 copySymbolSitac.setPayload(symbolSitac.getPayload());
-
                 copySymbolSitac.setIntervention(intervention);
 
                 listSymbAssociateWithIntervention.add(copySymbolSitac);
@@ -74,7 +85,7 @@ public class ListSigService implements SigRepository<SymbolSitac> {
     }
 
     /**
-     *Method to get the distance in meters between two positions
+     * Method to get the distance in meters between two positions
      *
      * @param pos1 The fist position
      * @param pos2 The second position
@@ -97,11 +108,14 @@ public class ListSigService implements SigRepository<SymbolSitac> {
         double diff2 = lng2 - lng1;
 
         //Partial Result
-        double partialResult1 = sin(diff1/2) * sin(diff2/2) +
+        double partialResult1 = sin(diff1 / 2) * sin(diff2 / 2) +
                 cos(lat1) * cos(lat2) * sin(diff2) * sin(diff2);
-        double partialResult2 = atan2(sqrt(partialResult1), sqrt(1 - partialResult1));
+
+        double partialResult2 = atan2(
+                sqrt(partialResult1),
+                sqrt(1 - partialResult1));
 
         //Result
-        return  r * partialResult2;
+        return r * partialResult2;
     }
 }
